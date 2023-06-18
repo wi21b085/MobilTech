35d5c6a4e2bd2a0c5e4475fc79dd2f4992d7a7a1
wi21b085 <wi21b085@technikum-wien.at>
2023-06-18 11:58:40 +0200

    viewCustomers fix

--- frontend/js/viewCustomers.js
+++ frontend/js/viewCustomers.js
@@ -1,103 +1,116 @@
-const username = document.cookie.split(';').find(cookie => cookie.includes('username')).split('=')[1];
+$(document).ready(function () {
 
-function ladeCustomer(customer) {
-    let table = $('<table>').addClass('table table-striped');
+    function ladeCustomer(customer) {
+        let row = $('<tr>').appendTo($('tbody'));
+        row.append($('<td>').text(customer.id));
+        row.append($('<td>').text(customer.vorname));
+        row.append($('<td>').text(customer.nachname));
+        row.append($('<td>').text(customer.username));
+        row.append($('<td>').text(customer.email));
+        row.append($('<td>').text(customer.adresse));
+        row.append($('<td>').text(customer.plz));
+        row.append($('<td>').text(customer.ort));
 
-    let thead = $('<thead>').appendTo(table);
-    let headerRow = $('<tr>').appendTo(thead);
-    headerRow.append($('<th>').text('ID'));
-    headerRow.append($('<th>').text('Vorname'));
-    headerRow.append($('<th>').text('Nachname'));
-    headerRow.append($('<th>').text('Username'));
-    headerRow.append($('<th>').text('Email'));
-    headerRow.append($('<th>').text('Adresse'));
-    headerRow.append($('<th>').text('PLZ'));
-    headerRow.append($('<th>').text('Ort'));
-    headerRow.append($('<th>').text('Status'));
-    headerRow.append($('<th>').text('Admin'));
+        let span = $("<span>")
 
-    let tableBody = $('<tbody>').appendTo(table);
-    let row = $('<tr>').appendTo(tableBody);
-    row.append($('<td>').text(customer.id));
-    row.append($('<td>').text(customer.vorname));
-    row.append($('<td>').text(customer.nachname));
-    row.append($('<td>').text(customer.username));
-    row.append($('<td>').text(customer.email));
-    row.append($('<td>').text(customer.adresse));
-    row.append($('<td>').text(customer.plz));
-    row.append($('<td>').text(customer.ort));
-
-    let span = $("<span>")
-
-    if (customer.username !== username) {
         span.on("click", function () {
-            $.ajax({
-                url: "../../backend/logic/requestHandler.php",
-                type: "POST",
-                data: {
-                    method: "statusUpdate",
-                    param: JSON.stringify({
-                        id: customer.id,
-                        status: customer.status
-                    })
-                },
-                success: function (response) {
-                    console.log(response.success)
-                    window.location.reload()
-                },
-                error: function (error) {
-                    console.log("Error on POST of Status Update")
-                    console.log(error)
-                    if (customer.status == 0) alert("Error on Activating")
-                    else alert("Error on Deactivating")
-                }
-            });
+            const username = document.cookie.split(';').find(cookie => cookie.includes('username')).split('=')[1];
+            if (customer.username !== username) {
+
+                $.ajax({
+                    url: "../../backend/logic/requestHandler.php",
+                    type: "POST",
+                    data: {
+                        method: "statusUpdate",
+                        param: JSON.stringify({
+                            id: customer.id,
+                            status: customer.status
+                        })
+                    },
+                    success: function (response) {
+                        console.log(response.success)
+                        window.location.reload()
+                    },
+                    error: function (error) {
+                        console.log("Error on POST of Status Update")
+                        console.log(error)
+                        if (customer.status == 0) alert("Error on Activating")
+                        else alert("Error on Deactivating")
+                    }
+                });
+            }
         })
-    }
 
-    if (customer.status == 0) {
-        var status = 'Inaktiv';
-        span.attr("class", "btn btn-danger");
-    }
-    else {
-        var status = 'Aktiv';
-        span.attr("class", "btn btn-success");
-    }
-
-    let td = $('<td style="text-align:center">');
-    row.append(td);
-    td.append(span.text(status));
-    {
-        if (customer.admin == 0) customer.admin = 'Nein';
-        else customer.admin = 'Ja';
-    }
-    row.append($('<td style="text-align:center">').text(customer.admin));
-
-    $('#viewCustomersTable').append(table);
-}
-
-let url = "../../backend/logic/requestHandler.php";
-
-let config = {
-    url: url,
-    type: "GET",
-    dataType: "json",
-    data: {
-        method: "viewCustomers"
-    },
-    success: function (response) {
-        console.log(response);
-        for (let i in response) {
-            let customer = response[i];
-            console.log(customer)
-            ladeCustomer(customer);
+        if (customer.status == 0) {
+            var status = 'Inaktiv';
+            span.attr("class", "btn btn-danger");
         }
-    },
-    error: function () {
-        $('.error').append('<center><div class="alert alert-danger" role="alert" style="width:50%;">The data could not be loaded! :(</div></center>');
+        else {
+            var status = 'Aktiv';
+            span.attr("class", "btn btn-success");
+        }
+
+        let td = $('<td style="text-align:center">');
+        row.append(td);
+        td.append(span.text(status));
+        {
+            if (customer.admin == 0) customer.admin = 'Nein';
+            else customer.admin = 'Ja';
+        }
+        row.append($('<td style="text-align:center">').text(customer.admin));
+
     }
-};
 
-$.ajax(config);
+    let url = "../../backend/logic/requestHandler.php";
 
-console.log("Skript geladen")
+    let config = {
+        url: url,
+        type: "GET",
+        dataType: "json",
+        data: {
+            method: "viewCustomers"
+        },
+        success: function (response) {
+            console.log(response);
+
+            if (response.admin !== "false") {
+                createTable()
+
+                for (let i in response) {
+                    let customer = response[i];
+                    console.log(customer)
+                    ladeCustomer(customer);
+                }
+            } else {
+                window.location = "index.html"
+            }
+        },
+        error: function () {
+            $('.error').append('<center><div class="alert alert-danger" role="alert" style="width:50%;">The data could not be loaded! :(</div></center>');
+        }
+    };
+
+    $.ajax(config);
+
+    function createTable() {
+        let table = $('<table>').addClass('table table-striped');
+
+        let thead = $('<thead>').appendTo(table);
+        let headerRow = $('<tr>').appendTo(thead);
+        headerRow.append($('<th>').text('ID'));
+        headerRow.append($('<th>').text('Vorname'));
+        headerRow.append($('<th>').text('Nachname'));
+        headerRow.append($('<th>').text('Username'));
+        headerRow.append($('<th>').text('Email'));
+        headerRow.append($('<th>').text('Adresse'));
+        headerRow.append($('<th>').text('PLZ'));
+        headerRow.append($('<th>').text('Ort'));
+        headerRow.append($('<th>').text('Status'));
+        headerRow.append($('<th>').text('Admin'));
+
+        $('<tbody>').appendTo(table);
+        $('#viewCustomersTable').append(table);
+    }
+
+    console.log("Skript geladen")
+})
